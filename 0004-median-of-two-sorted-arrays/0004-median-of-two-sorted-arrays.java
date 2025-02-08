ee09bdedf432da460144eb1d73ca78934ade691b
Shashi Vardhan <vardhans367@gmail.com>
2025-02-08 11:21:56 +0530

    Time: 1 ms (100%), Space: 46.3 MB (25.36%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,35 +1,31 @@
-import java.util.Arrays;
-
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        // Get the sizes of both input arrays.
         int n = nums1.length;
         int m = nums2.length;
+        int i = 0, j = 0, m1 = 0, m2 = 0;
 
-        // Merge the arrays into a single sorted array.
-        int[] merged = new int[n + m];
-        int k = 0;
-        for (int i = 0; i < n; i++) {
-            merged[k++] = nums1[i];
+        // Find median.
+        for (int count = 0; count <= (n + m) / 2; count++) {
+            m2 = m1;
+            if (i != n && j != m) {
+                if (nums1[i] > nums2[j]) {
+                    m1 = nums2[j++];
+                } else {
+                    m1 = nums1[i++];
+                }
+            } else if (i < n) {
+                m1 = nums1[i++];
+            } else {
+                m1 = nums2[j++];
+            }
         }
-        for (int i = 0; i < m; i++) {
-            merged[k++] = nums2[i];
-        }
 
-        // Sort the merged array.
-        Arrays.sort(merged);
-
-        // Calculate the total number of elements in the merged array.
-        int total = merged.length;
-
-        if (total % 2 == 1) {
-            // If the total number of elements is odd, return the middle element as the median.
-            return (double) merged[total / 2];
+        // Check if the sum of n and m is odd.
+        if ((n + m) % 2 == 1) {
+            return (double) m1;
         } else {
-            // If the total number of elements is even, calculate the average of the two middle elements as the median.
-            int middle1 = merged[total / 2 - 1];
-            int middle2 = merged[total / 2];
-            return ((double) middle1 + (double) middle2) / 2.0;
+            double ans = (double) m1 + (double) m2;
+            return ans / 2.0;
         }
     }
 }
