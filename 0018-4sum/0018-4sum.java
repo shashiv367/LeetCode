b13a30f99c8be9e2624cf1774958fec5575a0bf3
Shashi Vardhan <vardhans367@gmail.com>
2025-02-10 11:15:32 +0530

    Time: 87 ms (13.19%), Space: 45.3 MB (19.52%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,39 +1,40 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> li=new ArrayList<>();
-        if(nums==null || nums.length<4){
-            return li;
-        }
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length-3;i++){
-            if(i>0&&nums[i]==nums[i-1]){
-                continue;
-            }
-            for(int j=i+1;j<nums.length-2;j++){
-                if(j>i+1&&nums[j]==nums[j-1]){
-                    continue;
-                }
-                int left=j+1; int right=nums.length-1;
-                while(left<right){
-                    long sum=(long)nums[i]+nums[j]+nums[left]+nums[right];
-                    if(sum==target){
-                        li.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
-                        while(left<right&&nums[left]==nums[left+1]){
-                            left++;
-                        }
-                        while(left<right&&nums[right]==nums[right+-1]){
-                            right--;
-                        }
-                        left++;
-                        right--;
+       List<List<Integer>> res = new ArrayList<>();
+       Arrays.sort(nums);
+       HashSet<String> hs = new HashSet<>();
+
+       for(int i =0;i<nums.length-3;i++){
+            for(int j =i+1;j<nums.length-2;j++){
+                int ri = nums.length-1;
+                int li = j+1;
+                while(li<ri){
+                    long sum =0;
+                    sum+=nums[i];
+                    sum+=nums[j];
+                    sum+=nums[li];
+                    sum+=nums[ri];
+                    if(sum>target){
+                        ri--;
                     }else if(sum<target){
-                        left++;
+                        li++;
                     }else{
-                        right--;
+                        StringBuilder sb = new StringBuilder();
+                        sb.append(nums[i]);
+                        sb.append(nums[j]);
+                        sb.append(nums[li]);
+                        sb.append(nums[ri]);
+                        String match = sb.toString();
+                        if(hs.contains(match)==false){
+                            hs.add(match);
+                            res.add(Arrays.asList(nums[i], nums[j], nums[li], nums[ri]));
+                        }
+                        ri--;
+                        li++;   
                     }
                 }
             }
-        }
-        return li;
+       }
+       return res;
     }
 }
