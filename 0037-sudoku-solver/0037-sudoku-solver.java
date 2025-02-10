8d9bd02822451dd7a5610dfd50304d0c47479375
Shashi Vardhan <vardhans367@gmail.com>
2025-02-10 11:23:14 +0530

    Time: 209 ms (59.32%), Space: 41.2 MB (57.78%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -2,53 +2,61 @@ class Solution {
     public void solveSudoku(char[][] board) {
         solve(board);
     }
-
+    
     private boolean solve(char[][] board) {
+        // Iterate over each cell in the board
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                if (board[i][j] == '.') {  // Find an empty cell
-                    for (char c = '1'; c <= '9'; c++) {
-                        if (isValid(board, i, j, c)) {  // Check if placing the number is valid
-                            board[i][j] = c;  // Place the number
-                            if (solve(board)) {  // Recursively try to solve the rest of the board
-                                return true;
+                // If the current cell is empty (i.e., it is '.')
+                if (board[i][j] == '.') {
+                    // Try placing digits 1 to 9
+                    for (char num = '1'; num <= '9'; num++) {
+                        // Check if it's valid to place the number at (i, j)
+                        if (isValid(board, i, j, num)) {
+                            board[i][j] = num; // Place the number
+                            
+                            // Recurse to solve the next part of the board
+                            if (solve(board)) {
+                                return true; // If the board is solved, return true
                             }
-                            board[i][j] = '.';  // Backtrack if it doesn't work
+                            
+                            // If placing the number didn't lead to a solution, backtrack
+                            board[i][j] = '.'; 
                         }
                     }
-                    return false;  // If no number is valid, return false to backtrack
+                    return false; // If no number can be placed, return false (backtrack)
                 }
             }
         }
-        return true;  // If the whole board is filled correctly
+        return true; // If all cells are filled correctly, return true
     }
-
-    private boolean isValid(char[][] board, int row, int col, char c) {
-        // Check row
+    
+    private boolean isValid(char[][] board, int row, int col, char num) {
+        // Check the row
         for (int i = 0; i < 9; i++) {
-            if (board[row][i] == c) {
+            if (board[row][i] == num) {
                 return false;
             }
         }
         
-        // Check column
+        // Check the column
         for (int i = 0; i < 9; i++) {
-            if (board[i][col] == c) {
+            if (board[i][col] == num) {
                 return false;
             }
         }
-
-        // Check 3x3 subgrid
+        
+        // Check the 3x3 subgrid
         int startRow = (row / 3) * 3;
         int startCol = (col / 3) * 3;
         for (int i = startRow; i < startRow + 3; i++) {
             for (int j = startCol; j < startCol + 3; j++) {
-                if (board[i][j] == c) {
+                if (board[i][j] == num) {
                     return false;
                 }
             }
         }
         
-        return true;  // Valid placement
+        return true; // If no conflicts, the number can be placed
     }
 }
