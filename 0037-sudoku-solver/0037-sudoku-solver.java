f4318b7f8cc4f6316eaa4679af1157e3823c2e8d
Shashi Vardhan <vardhans367@gmail.com>
2025-02-10 11:24:00 +0530

    Time: 59 ms (94.2%), Space: 41 MB (71.84%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,62 +1,62 @@
 class Solution {
     public void solveSudoku(char[][] board) {
-        solve(board);
-    }
-    
-    private boolean solve(char[][] board) {
-        // Iterate over each cell in the board
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                // If the current cell is empty (i.e., it is '.')
-                if (board[i][j] == '.') {
-                    // Try placing digits 1 to 9
-                    for (char num = '1'; num <= '9'; num++) {
-                        // Check if it's valid to place the number at (i, j)
-                        if (isValid(board, i, j, num)) {
-                            board[i][j] = num; // Place the number
-                            
-                            // Recurse to solve the next part of the board
-                            if (solve(board)) {
-                                return true; // If the board is solved, return true
-                            }
-                            
-                            // If placing the number didn't lead to a solution, backtrack
-                            board[i][j] = '.'; 
-                        }
-                    }
-                    return false; // If no number can be placed, return false (backtrack)
+        // Bitmask to store used digits in rows, columns, and 3x3 subgrids
+        int[] rows = new int[9];
+        int[] cols = new int[9];
+        int[] boxes = new int[9];
+
+        // Pre-process the board to initialize the bitmask arrays
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                if (board[r][c] != '.') {
+                    int num = board[r][c] - '1'; // Convert '1' to 0, '9' to 8
+                    rows[r] |= (1 << num);
+                    cols[c] |= (1 << num);
+                    boxes[(r / 3) * 3 + (c / 3)] |= (1 << num);
                 }
             }
         }
-        return true; // If all cells are filled correctly, return true
+
+        // Start the backtracking process
+        solve(board, rows, cols, boxes, 0, 0);
     }
-    
-    private boolean isValid(char[][] board, int row, int col, char num) {
-        // Check the row
-        for (int i = 0; i < 9; i++) {
-            if (board[row][i] == num) {
-                return false;
-            }
-        }
+
+    private boolean solve(char[][] board, int[] rows, int[] cols, int[] boxes, int r, int c) {
+        // If we reached the end of the board
+        if (r == 9) return true;
         
-        // Check the column
-        for (int i = 0; i < 9; i++) {
-            if (board[i][col] == num) {
-                return false;
-            }
-        }
+        // If we reached the end of the row, move to the next row
+        if (c == 9) return solve(board, rows, cols, boxes, r + 1, 0);
         
-        // Check the 3x3 subgrid
-        int startRow = (row / 3) * 3;
-        int startCol = (col / 3) * 3;
-        for (int i = startRow; i < startRow + 3; i++) {
-            for (int j = startCol; j < startCol + 3; j++) {
-                if (board[i][j] == num) {
-                    return false;
+        // Skip filled cells
+        if (board[r][c] != '.') return solve(board, rows, cols, boxes, r, c + 1);
+
+        // Try placing digits 1-9
+        for (int num = 0; num < 9; num++) {
+            // Calculate the bitmask for the current number
+            int boxIdx = (r / 3) * 3 + (c / 3);
+            
+            // Check if placing the current number is valid
+            if ((rows[r] & (1 << num)) == 0 && (cols[c] & (1 << num)) == 0 && (boxes[boxIdx] & (1 << num)) == 0) {
+                // Place the number
+                board[r][c] = (char) (num + '1');
+                rows[r] |= (1 << num);
+                cols[c] |= (1 << num);
+                boxes[boxIdx] |= (1 << num);
+
+                // Recursively attempt to solve the rest of the board
+                if (solve(board, rows, cols, boxes, r, c + 1)) {
+                    return true;
                 }
+
+                // Backtrack
+                board[r][c] = '.';
+                rows[r] &= ~(1 << num);
+                cols[c] &= ~(1 << num);
+                boxes[boxIdx] &= ~(1 << num);
             }
         }
-        
-        return true; // If no conflicts, the number can be placed
+
+        return false; // If no valid number can be placed, return false
     }
 }
