9539cae86f72b8c5baf1ef5c31e1992095bab460
Shashi Vardhan <vardhans367@gmail.com>
2025-02-10 11:00:38 +0530

    Time: 4 ms (69.08%), Space: 44.8 MB (52.28%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,28 +1,24 @@
 class Solution {
     public int romanToInt(String s) {
-        Map<Character, Integer> roman = new HashMap<>();
-        roman.put('I', 1);
-        roman.put('V', 5);
-        roman.put('X', 10);
-        roman.put('L', 50);
-        roman.put('C', 100);
-        roman.put('D', 500);
-        roman.put('M', 1000);
+        Map<Character, Integer> roman = Map.of(
+            'I', 1, 'V', 5, 'X', 10, 'L', 50, 'C', 100, 'D', 500, 'M', 1000
+        );
         
         int total = 0;
         int prev = 0;
         
-        // Traverse from right to left
+        // Iterate from right to left (more efficient for subtraction logic)
         for (int i = s.length() - 1; i >= 0; i--) {
             int curr = roman.get(s.charAt(i));
-            
+
+            // If current numeral is smaller than the previous, subtract it
             if (curr < prev) {
-                total -= curr;  // Subtract if it's a smaller numeral before a larger one
+                total -= curr;
             } else {
                 total += curr;
             }
-            
-            prev = curr;  // Update previous value
+
+            prev = curr; // Update previous numeral
         }
         
         return total;
