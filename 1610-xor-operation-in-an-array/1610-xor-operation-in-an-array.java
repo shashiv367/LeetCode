368bf275fd779a0dd9b247a9b8de51ca68455b6b
Shashi Vardhan <vardhans367@gmail.com>
2025-02-11 11:14:04 +0530

    Time: 0 ms (100%), Space: 40.2 MB (91.94%) - LeetHub

--- 1610-xor-operation-in-an-array/1610-xor-operation-in-an-array.java
+++ 1610-xor-operation-in-an-array/1610-xor-operation-in-an-array.java
@@ -1,15 +1,9 @@
 class Solution {
     public int xorOperation(int n, int start) {
-        int result = 0;
-        for (int i = 0; i < n; i++) {
-            result ^= start + 2 * i;
+        int xorData = start;
+        for(int i=1;i<n;i++){
+            xorData ^= (start + 2 * i);
         }
-        return result;
-    }
-
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        System.out.println(sol.xorOperation(5, 0)); // Output: 8
-        System.out.println(sol.xorOperation(4, 3)); // Output: 8
+    return xorData;
     }
 }
