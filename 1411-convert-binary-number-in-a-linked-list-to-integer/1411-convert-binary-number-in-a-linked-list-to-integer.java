4f6310735050210002f5374afc1a1c6900f60183
Shashi Vardhan <vardhans367@gmail.com>
2025-03-03 11:29:40 +0530

    Time: 0 ms (100%), Space: 40.9 MB (88.02%) - LeetHub

--- 1411-convert-binary-number-in-a-linked-list-to-integer/1411-convert-binary-number-in-a-linked-list-to-integer.java
+++ 1411-convert-binary-number-in-a-linked-list-to-integer/1411-convert-binary-number-in-a-linked-list-to-integer.java
@@ -11,10 +11,9 @@
 class Solution {
     public int getDecimalValue(ListNode head) {
         StringBuilder sb=new StringBuilder();
-        ListNode temp=head;
-        while(temp!=null){
-            sb.append(temp.val);
-            temp=temp.next;
+        while(head!=null){
+            sb.append(head.val);
+            head=head.next;
         }
         int decimal = Integer.parseInt(sb.toString(), 2);
         return decimal;
